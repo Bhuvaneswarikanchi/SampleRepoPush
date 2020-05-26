895c1804cb2240ede574291028bc4299b8b94504
Bhuna <bhuvaneshwarikanchi@gmail.com>
2020-05-26 10:19:53 +0530

    first commit

---
+++ src/Adding.java
@@ -0,0 +1,15 @@
+
+public class Adding {
+	public static void main(String[] args)
+	{
+		int a,b,res;
+		res=add(3,4);
+		System.out.println("addition result:"+res);
+	}
+	
+	 static int add(int a,int b)
+	 {
+		return a+b;
+	}
+
+}
